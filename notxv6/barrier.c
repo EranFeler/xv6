e96df9e205c88d4116638725a824c4d418c1ad01
EranFeler <eranfeler@gmail.com>
2026-07-01 14:50:14 +0300

    solved

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -25,12 +25,18 @@ barrier_init(void)
 static void 
 barrier()
 {
-  // YOUR CODE HERE
-  //
-  // Block until all threads have called barrier() and
-  // then increment bstate.round.
-  //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  int this_threads_round = bstate.round;
+  bstate.nthread++;
+  if (bstate.nthread < nthread) {
+      while (this_threads_round == bstate.round)
+          pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  } else {
+      bstate.nthread = 0;
+      bstate.round++;
+      pthread_cond_broadcast(&bstate.barrier_cond);
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
